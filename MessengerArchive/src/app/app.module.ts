1a04d4c26e73503f9ef71aaa8d471fa812863e04
Thomas Drescher <drescher.thomas@outlook.com>
2018-08-01 17:31:17 +0200

    Updated component (minor changes html)

--- MessengerArchive/src/app/app.module.ts
+++ MessengerArchive/src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './components/root/app.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { OverviewComponent } from './components/overview/overview.component';
+import { SettingsComponent } from './settings/settings.component';
+import { HelpComponent } from './help/help.component';
+import { AoutComponent } from './aout/aout.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    OverviewComponent
+    OverviewComponent,
+    SettingsComponent,
+    HelpComponent,
+    AoutComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
