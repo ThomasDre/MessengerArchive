b413b01a9bf692331dd91925890ce2bae94d21bb
Thomas Drescher <drescher.thomas@outlook.com>
2018-08-01 14:52:53 +0200

    Added main components
    No implementation yet

--- MessengerArchive/src/app/app.module.ts
+++ MessengerArchive/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/root/app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { OverviewComponent } from './components/overview/overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule
